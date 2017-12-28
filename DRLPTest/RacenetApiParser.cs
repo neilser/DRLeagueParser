4f10e3a2d411f1123ddda72cc66b34966a0d228f
Neil <Neil@BIGBLACKTOWER>
2017-12-28 18:35:29 +0000

    Remove unnecessary "uk" in URL to fetch rally data

--- DRLPTest/RacenetApiParser.cs
+++ DRLPTest/RacenetApiParser.cs
@@ -42,7 +42,7 @@ namespace DRLPTest
                 // example string for reference
                 //string apiUrl = "https://www.dirtgame.com/uk/api/event?assists=any&eventId=95576&group=all&leaderboard=true&nameSearch=&noCache=1463699433315&number=10&page=1&stageId=0&wheel=any";
 
-                string apiUrl = "https://www.dirtgame.com/uk/api/event?assists=any&group=all&leaderboard=true&nameSearch=&number=10&wheel=any";
+                string apiUrl = "https://www.dirtgame.com/api/event?assists=any&group=all&leaderboard=true&nameSearch=&number=10&wheel=any";
 
                 var uriBuilder = new UriBuilder(apiUrl);
                 var query = HttpUtility.ParseQueryString(uriBuilder.Query);
