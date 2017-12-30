cd9ee5d44fe6b3955bc12688d58f685595431d28
Neil <Neil@BIGBLACKTOWER>
2017-12-30 23:47:11 +0000

    Mods to command line program. Now outputs PlayerID etc. Not fully tested yet - need to try on a large event with missing times etc.

--- DRLPCommandLine/Program.cs
+++ DRLPCommandLine/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using DRNumberCrunchers;
 using DRLPTest;
@@ -35,7 +35,7 @@ namespace DRLPCommandLine
                 if (args[i] == "-eid")
                     EventID = int.Parse(args[i + 1].Trim());
             }
-
+            // TODO: print a full "usage" message if no arguments provided, or problems with the arguments
             /*if (LeagueURL == string.Empty)
             {
                 WriteErrorFile("No eventID provided\nPlease provide an ID with the -lurl \"address\" parameter");
@@ -48,7 +48,7 @@ namespace DRLPCommandLine
                 return;
             }
 
-            Console.WriteLine("Parsing league data from league ID " + EventID.ToString());
+            Console.WriteLine("Parsing data from event ID " + EventID.ToString());
 
             rallyData = new Rally();
 
@@ -69,61 +69,80 @@ namespace DRLPCommandLine
             rallyData.CalculateTimes();
 
             // Got the result, now lets sort it into the format we want!
-            int numStages = 0;
-            int numDrivers = 0;
 
-            List<string> driverNames = new List<string>();
-            List<string> driverVehicles = new List<string>();
-            foreach (Stage a in rallyData)
+            // original code basically uses three loops (!) to output the list of stage times for each driver (one driver per line):
+            // first loop runs through the stages and net result is: number of stages and drivers are determined; driverNames & Vehicles (+ PlayerIDs now)
+            // are extracted
+            // second loop runs through stages and extracts the per-driver times to driver-specific DriverTime lists
+            // third loop runs through the drivers and prints names, etc., then all stage times for that driver on one line
+            // NB: the sequence of lines in the output file has no relation to the stage or overall times as far as I can see
+
+            // want to replace this with one loop if poss, with output being similar (one driver per line) but including PlayerID and ProfileURL,
+            // and also with sorting by overall rally time:
+            // take final stage data, sort it, get the keys (PlayerIDs) from that sorted list
+            // iterate over the sorted PlayerIDs and print required driver data + times from all stages 
+
+            // Get the final stage data for the rally 
+
+            var StageEnum = ((IEnumerable)rallyData).GetEnumerator();
+
+            // OK, massive argh, not loving C# right now - can't find a way to do this cleanly. Only the Generic Lists have a Last() method,
+            // so I have to do stupid crap like advancing the enumerator several times to get to the last stage. Beyond crap. The only other option
+            // I could see was to make the stage data public (currently private). Basically I'm a C# noob and plan to keep it that way after this
+            // experience.
+            int argh;
+            for (argh = 0; argh < rallyData.StageCount; argh++) StageEnum.MoveNext();
+            //WriteErrorFile(argh.ToString());
+            Stage finalStage = (Stage)StageEnum.Current;
+
+            // Now sort it by overall time (pinched various bits of the code below from printOverallTimes() in DRParserTest.xaml.cs)
+            List<KeyValuePair<int, DriverTime>> sortedFinalStageData = finalStage.DriverTimes.ToList();
+
+            sortedFinalStageData.Sort((x, y) =>
             {
-                numStages++;
-                if (numDrivers == 0)
-                {
-                    foreach (KeyValuePair<string, DriverTime> b in a.DriverTimes)
-                    {
-                        driverNames.Add(b.Key);
-                        driverVehicles.Add(b.Value.Vehicle);
-                        numDrivers++;
-                    }
-                }
-            }
+                if (x.Value != null && y.Value == null)
+                    return -1;
+                else if (x.Value == null && y.Value != null)
+                    return 1;
+                else if (x.Value == null && y.Value == null)
+                    return 0;
+                else
+                    return x.Value.CalculatedOverallTime.CompareTo(y.Value.CalculatedOverallTime);
+            });
 
-            List<DriverTime>[] Times = new List<DriverTime>[numDrivers];
-            for(int i = 0; i < numDrivers; i++)
-                Times[i] = new List<DriverTime>();
-
-            int driverIndex = 0;
-            int stageIndex = 0;
-            foreach (Stage a in rallyData)
-            {
-                driverIndex = 0;
-                foreach (String b in driverNames) {
-                    Times[driverIndex].Add(a.DriverTimes[b]);
-                    driverIndex++;
-                }
-            }
-
+            // And finally, iterate over all drivers and generate our output file
             string outPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-
             System.IO.Directory.CreateDirectory(outPath + "\\DRLPResults\\");
-            //+DateTime.Now.ToString("") + ".csv"
-            System.IO.StreamWriter fout = new System.IO.StreamWriter(outPath + "\\DRLPResults\\DRLPRes_" + DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss") + ".csv");
-            driverIndex = 0;
-            foreach(string a in driverNames)
-            {
-                fout.Write(a + ",");
-                fout.Write(driverVehicles[driverIndex] + ",");
-                foreach (DriverTime t in Times[driverIndex])
-                {
-                    if(t != null)
-                        fout.Write(t.CalculatedStageTime.TotalSeconds.ToString("0.000"));
-                    fout.Write(",");
-                }
-                fout.Write("\n");
-                driverIndex++;
-            }
-            fout.Close();
+            System.IO.StreamWriter fout = new System.IO.StreamWriter(outPath + "\\DRLPResults\\DRLPRes_" + EventID.ToString() + "_" +
+                DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss") + ".csv");
             
+            foreach (KeyValuePair<int, DriverTime> driverTimeKvp in sortedFinalStageData)
+            {
+                var driverPlayerID = driverTimeKvp.Key;
+                var driverTime = driverTimeKvp.Value;
+
+                // generate the driver info
+                var line =
+                        driverTime.PlayerID + "," +
+                        "\"" + driverTime.DriverName + "\"" + "," +
+                        driverTime.ProfileURL + "," +
+                        driverTime.Vehicle + ",";
+
+                // and now all stage times
+                foreach (Stage stage in rallyData)
+                {
+                    DriverTime stageTime = stage.DriverTimes[driverPlayerID];
+                    if (stageTime != null) line += stageTime.CalculatedStageTime.TotalSeconds.ToString("0.000");
+                    line += ",";
+                }
+                line += "\n";
+
+                // and write the output to the file
+                fout.Write(line);
+            }
+
+            fout.Close();
+            Console.WriteLine("Output written");
             return;
         }
     }
